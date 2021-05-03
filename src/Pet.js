9c63baa7e0bb6b0a3525f46f74259b32de0358f1
Minjie Huang <millerk1029@gmail.com>
2021-05-03 15:36:13 +1000

    Add Modal

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 // const Pet = props => {
 //   return (
@@ -17,16 +18,16 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
 
       <div className="info">
         <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <h2>{`${animal} - ${breed} - ${location} `}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
